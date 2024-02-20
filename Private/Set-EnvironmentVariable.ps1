04fb38b0b2576038567e8f4c15138dd64002dc05
Alain Herve <alainQtec@gmail.com>
2024-02-20 11:43:58 +0200

    Create Set-EnvironmentVariable.ps1

---
+++ Private/Set-EnvironmentVariable.ps1
@@ -0,0 +1,13 @@
+﻿function Set-EnvironmentVariable {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(Position = 0)]
+        [String]$Name,
+
+        [parameter(Position = 1, ValueFromRemainingArguments)]
+        [String[]]$Value
+    )
+    $FullVal = $Value -join " "
+    Write-BuildLog "Setting env variable '$Name' to '$fullVal'"
+    Set-Item -Path ([IO.Path]::Combine('Env:', $Name)) -Value $FullVal -Force
+}
