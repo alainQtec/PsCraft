855c689036a34b6161279f6ec66db99a8b41ab39
Alain Herve <alainQtec@gmail.com>
2024-06-21 17:45:18 +0200

    Update Get-PsModule.ps1

--- Public/Get-PsModule.ps1
+++ Public/Get-PsModule.ps1
@@ -1,19 +1,19 @@
 function Get-PsModule {
-    <#
-    .SYNOPSIS
-    Gets a psmodule object, by creating one from scratch or Loading One from already Existing Files
-    .DESCRIPTION
-    A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
-    .EXAMPLE
-    Get-PsModule -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
-    #>
+
+    # .SYNOPSIS
+    #   Gets a psmodule object, by creating one from scratch or Loading One from already Existing Files
+    # .DESCRIPTION
+    #   A longer description of the function, its purpose, common use cases, etc.
+    # .NOTES
+    #   Information or caveats about the function e.g. 'This function is not supported in Linux'
+    # .LINK
+    #   Specify a URI to a help page, this will show when Get-Help -Online is used.
+    # .EXAMPLE
+    #   Get-PsModule -Verbose
+    #   Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    # .OUTPUTS
+    #   [PSmodule]
     [CmdletBinding(DefaultParametersetName = 'ByName')]
-    [OutputType([PSmodule])]
     param (
         [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'ByName')]
         [ValidateNotNullOrEmpty()]
