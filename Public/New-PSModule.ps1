8d07863a68ec599d12e9b71af6557b75460b64ca
Alain Herve <alainQtec@gmail.com>
2024-06-21 17:45:35 +0200

    Update New-PSModule.ps1
    lets do this one more time ...

--- Public/New-PSModule.ps1
+++ Public/New-PSModule.ps1
@@ -1,19 +1,19 @@
 function New-PSModule {
-    <#
-    .SYNOPSIS
-    Creates/Writes a psmodule Object On disk
-    .DESCRIPTION
-    A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
-    .EXAMPLE
-    New-PSModule -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
-    #>
+
+    # .SYNOPSIS
+    #   Creates/Writes a psmodule Object On disk
+    # .DESCRIPTION
+    #   A longer description of the function, its purpose, common use cases, etc.
+    # .NOTES
+    #   Information or caveats about the function e.g. 'This function is not supported in Linux'
+    # .LINK
+    #   Specify a URI to a help page, this will show when Get-Help -Online is used.
+    # .EXAMPLE
+    #   New-PSModule -Verbose
+    #   Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    # .OUTPUTS
+    #   [PSmodule]
     [CmdletBinding(SupportsShouldProcess, DefaultParametersetName = 'ByName')]
-    [OutputType([PSmodule])]
     param (
         # The Name Of your Module; note that it Should always match BaseName of its path.
         [Parameter(Position = 0, ParameterSetName = 'ByName')]
