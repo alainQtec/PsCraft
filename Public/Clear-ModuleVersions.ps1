27525b690b9acce49055996fbf23bc47d1f80922
Alain Herve <alainQtec@gmail.com>
2024-10-11 12:52:31 +0200

    Create Clear-ModuleVersions.ps1

---
+++ Public/Clear-ModuleVersions.ps1
@@ -0,0 +1,19 @@
+﻿function Clear-ModuleVersions {
+  # .SYNOPSIS
+  #   Cleans up outdated or unnecessary module versions.
+  #   & auto-removes duplicate versions.
+  [CmdletBinding()]
+  [Alias('Clean-ModuleVersions')]
+  param (
+  )
+
+  begin {
+  }
+
+  process {
+
+  }
+
+  end {
+  }
+}
