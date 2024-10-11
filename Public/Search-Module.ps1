a17c686645640ce4d4d7d85ebc4eccbd3d17b9fe
Alain Herve <alainQtec@gmail.com>
2024-10-11 12:52:58 +0200

    Create Search-Module.ps1

---
+++ Public/Search-Module.ps1
@@ -0,0 +1,23 @@
+﻿function Search-Module {
+  <#
+  .SYNOPSIS
+    Broader function to search specific repositories for modules.
+  .DESCRIPTION
+    Searches for modules in the specified repositories (local or online)
+  #>
+  [CmdletBinding()]
+  param (
+  )
+
+  begin {
+
+  }
+
+  process {
+
+  }
+
+  end {
+
+  }
+}
