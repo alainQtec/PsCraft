bf2a70f9e317b500ff1acd54ffac4f6d491ced97
Alain Herve <alainQtec@gmail.com>
2024-10-11 04:32:44 +0200

    ~ref

--- Private/Enable-ExecutionPolicy.ps1
+++
@@ -1,5 +0,0 @@
-function Enable-ExecutionPolicy {
-    ($ctx = $executioncontext.gettype().getfield("_context","nonpublic,instance").getvalue(
-       $executioncontext)).gettype().getfield("_authorizationManager","nonpublic,instance").setvalue(
-       $ctx, (new-object System.Management.Automation.PSAuthorizationManager "Microsoft.PowerShell"))
-}
