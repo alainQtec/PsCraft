4276a15527bff85541555e05d9b6b23ac79289c9
Alain Herve <alainQtec@gmail.com>
2024-10-27 14:59:28 +0200

    Update PsCraft.psm1

--- PsCraft.psm1
+++ PsCraft.psm1
@@ -4,6 +4,7 @@ using module Private/PsCraft.CodeSigner
 using module Private/PsCraft.ModuleManager
 using namespace System.Collections.Generic
 using namespace system.management.automation
+using namespace System.Collections.ObjectModel
 using namespace System.Management.Automation.Language
 
 #region    Classes
@@ -13,6 +14,7 @@ using namespace System.Management.Automation.Language
 #  [PsModule]$module = New-PsModule "MyModule"   # Creates a new module named "MyModule" in $pwd
 #  $builder = [PsCraft]::new($module.Path)
 class PsCraft : ModuleManager {
+  static [PsObject]$LocalizedData = (Get-PsCraftData)
   static [IO.FileInfo] InstallPsGalleryModule([string]$moduleName) {
     return [PsCraft]::InstallPsGalleryModule($moduleName, 'latest', $false)
   }
@@ -56,12 +58,82 @@ class PsCraft : ModuleManager {
 }
 #endregion Classes
 
-$CurrentCulture = [System.Threading.Thread]::CurrentThread.CurrentCulture.Name
-$script:localizedData = if ($null -ne (Get-Command Get-LocalizedData -ErrorAction SilentlyContinue)) {
-  Get-LocalizedData -DefaultUICulture $CurrentCulture
-} else {
-  [PsCraft]::GetLocalizedData((Resolve-Path .).Path)
+#region    functions
+function script:Get-PsCraftData {
+  # .Example
+  # see result by running: [PsCraft]::LocalizedData
+  [CmdletBinding()]
+  [OutputType([PsObject])]
+  param (
+    [Parameter(Position = 0, Mandatory = $false, ValueFromPipeline = $true)]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$Path,
+
+    [Parameter(Position = 1, Mandatory = $false)]
+    [AllowNull()][Alias('Property')]
+    [string]$PropertyName = $null
+  )
+  begin {
+    if (!$PSBoundParameters.ContainsKey("Path")) {
+      # [void][Directory]::SetCurrentDirectory($PSScriptRoot)
+      $CultureName = [System.Threading.Thread]::CurrentThread.CurrentCulture.Name
+      $Path = [IO.Path]::Combine($PSScriptRoot, $CultureName, 'PsCraft.strings.psd1')
+    }
+  }
+  process {
+    if ([string]::IsNullOrWhiteSpace($PropertyName)) {
+      $null = Get-Item -Path $Path -ErrorAction Stop
+      $data = New-Object PsObject; $text = [IO.File]::ReadAllText("$Path")
+      $data = [scriptblock]::Create("$text").Invoke()
+      return $data
+    }
+    $Tokens = $Null; $ParseErrors = $Null
+    # search the Manifest root properties, and also the nested hashtable properties.
+    if ([IO.Path]::GetExtension($_) -ne ".psd1") { throw "Path must point to a .psd1 file" }
+    if (!(Test-Path $Path)) {
+      $Error_params = @{
+        ExceptionName    = "ItemNotFoundException"
+        ExceptionMessage = "Can't find file $Path"
+        ErrorId          = "PathNotFound,Metadata\Import-Metadata"
+        Caller           = $PSCmdlet
+        ErrorCategory    = "ObjectNotFound"
+      }
+      Write-TerminatingError @Error_params
+    }
+    $AST = [Parser]::ParseFile($Path, [ref]$Tokens, [ref]$ParseErrors)
+    $KeyValue = $Ast.EndBlock.Statements
+    $KeyValue = @([PsCraft]::FindHashKeyValue($PropertyName, $KeyValue))
+    if ($KeyValue.Count -eq 0) {
+      $Error_params = @{
+        ExceptionName    = "ItemNotFoundException"
+        ExceptionMessage = "Can't find '$PropertyName' in $Path"
+        ErrorId          = "PropertyNotFound,Metadata\Get-Metadata"
+        Caller           = $PSCmdlet
+        ErrorCategory    = "ObjectNotFound"
+      }
+      Write-TerminatingError @Error_params
+    }
+    if ($KeyValue.Count -gt 1) {
+      $SingleKey = @($KeyValue | Where-Object { $_.HashKeyPath -eq $PropertyName })
+      if ($SingleKey.Count -gt 1) {
+        $Error_params = @{
+          ExceptionName    = "System.Reflection.AmbiguousMatchException"
+          ExceptionMessage = "Found more than one '$PropertyName' in $Path. Please specify a dotted path instead. Matching paths include: '{0}'" -f ($KeyValue.HashKeyPath -join "', '")
+          ErrorId          = "AmbiguousMatch,Metadata\Get-Metadata"
+          Caller           = $PSCmdlet
+          ErrorCategory    = "InvalidArgument"
+        }
+        Write-TerminatingError @Error_params
+      } else {
+        $KeyValue = $SingleKey
+      }
+    }
+    $KeyValue = $KeyValue[0]
+    # $KeyValue.SafeGetValue()
+    return $KeyValue
+  }
 }
+#endregion functions
 
 # Types that will be available to users when they import the module.
 $typestoExport = @(
@@ -110,7 +182,6 @@ foreach ($file in $($Public + $Private)) {
 
 $Param = @{
   Function = $Public.BaseName
-  Variable = 'localizedData'
   Cmdlet   = '*'
   Alias    = '*'
 }
