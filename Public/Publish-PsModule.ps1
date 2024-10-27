7627c4b05a942ab9cfb832f31c610cc8c57f9697
Alain Herve <alainQtec@gmail.com>
2024-10-27 14:59:51 +0200

    Update Publish-PsModule.ps1

--- Public/Publish-PsModule.ps1
+++ Public/Publish-PsModule.ps1
@@ -4,7 +4,7 @@ function Publish-PsModule {
   # .DESCRIPTION
   #   A longer description of the function, its purpose, common use cases, etc.
   # .NOTES
-  #   Information or caveats about the function e.g. 'This function is not supported in Linux'
+  #   Inspired by the module: https://github.com/gaelcolas/Sampler
   # .LINK
   #   Specify a URI to a help page, this will show when Get-Help -Online is used.
   # .EXAMPLE
