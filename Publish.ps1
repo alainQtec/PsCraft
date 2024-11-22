35a002c6d6e3bcea0fdacc2c563f67a083e66134
Alain Herve <alainQtec@gmail.com>
2024-11-22 16:14:56 +0200

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -39,18 +39,18 @@ Param()
 # $BuildScript = [IO.Path]::Combine($PSScriptRoot, 'build.ps1')
 $NugetApiKey = $Env:NUGETAPIKEY
 if (!$Env:GITHUB_ACTION_PATH) {
-    # Means, We are on local pc so we'll have to decrypt the env variable
-    $private:AESKey = $null; Set-Variable -Name AESKey -Value ([AzureHSM]::New().RetrieveKey()) -Option ReadOnly -Visibility Private
-    $apiPrompt_text = "Enter your NUGET APIKEY"
-    $private:UsrNpt = $null; Set-Variable -Name UsrNpt -Scope Local -Visibility Private -Option Private -Value $(if ([System.Environment]::OSVersion.Platform -in ('Win32NT', 'Win32S', 'Win32Windows', 'WinCE')) {
-            [AzureHSM]::ConvertToSecureString([scriptblock]::Create("$((Invoke-RestMethod -Method Get https://api.github.com/gists/b12ce490d427a917d3d6a24f71180f7e).files.'SecureText-Prompt-Gui.ps1'.content)").Invoke($apiPrompt_text))
-        } else {
-            Read-Host -AsSecureString -Prompt $apiPrompt_text
-        }
-    )
-    if (![regex]::IsMatch($NugetApiKey, '^(?=.{100,})(?=.*0{2,})')) {
-        Set-Item -Path ([IO.Path]::Combine('Env:', 'NUGETAPIKEY')) -Value $(ConvertFrom-SecureString -SecureString $usrNpt -Key $AESKey) -Force
-    }; $NugetApiKey = [system.Management.Automation.PSCredential]::New("test", $(ConvertTo-SecureString $Env:NUGETAPIKEY -Key $AESKey)).GetNetworkCredential().Password
+  # Means, We are on local pc so we'll have to decrypt the env variable
+  $private:AESKey = $null; Set-Variable -Name AESKey -Value ([AzureHSM]::New().RetrieveKey()) -Option ReadOnly -Visibility Private
+  $apiPrompt_text = "Enter your NUGET APIKEY"
+  $private:UsrNpt = $null; Set-Variable -Name UsrNpt -Scope Local -Visibility Private -Option Private -Value $(if ([System.Environment]::OSVersion.Platform -in ('Win32NT', 'Win32S', 'Win32Windows', 'WinCE')) {
+      [AzureHSM]::ConvertToSecureString([scriptblock]::Create("$((Invoke-RestMethod -Method Get https://api.github.com/gists/b12ce490d427a917d3d6a24f71180f7e).files.'SecureText-Prompt-Gui.ps1'.content)").Invoke($apiPrompt_text))
+    } else {
+      Read-Host -AsSecureString -Prompt $apiPrompt_text
+    }
+  )
+  if (![regex]::IsMatch($NugetApiKey, '^(?=.{100,})(?=.*0{2,})')) {
+    Set-Item -Path ([IO.Path]::Combine('Env:', 'NUGETAPIKEY')) -Value $(ConvertFrom-SecureString -SecureString $usrNpt -Key $AESKey) -Force
+  }; $NugetApiKey = [system.Management.Automation.PSCredential]::New("test", $(ConvertTo-SecureString $Env:NUGETAPIKEY -Key $AESKey)).GetNetworkCredential().Password
 }
 Write-Host "Your NugetApiKey: $NugetApiKey" -ForegroundColor Magenta
 # & $BuildScript -Task Deploy -ApiKey $NugetApiKey
