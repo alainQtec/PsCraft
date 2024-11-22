244a8cf0e2a1b8f5230b5aab1b6b2ee96cf51b13
Alain Herve <alainQtec@gmail.com>
2024-11-22 22:34:02 +0200

    Update New-PSModule.ps1

--- Public/ModuleManager/New-PSModule.ps1
+++ Public/ModuleManager/New-PSModule.ps1
@@ -6,10 +6,10 @@ function New-PsModule {
   #   In one use case, its just a simplified wrapper for New-ModuleManifest which answers some of the parameters based on the files already in the module folder.
   #   In the second use case, it allows you to collect one or more scripts and put them into a new module folder.
   # .LINK
-  #   https://github.com/alainQtec/PsCraft/blob/nain/Public/New-PsModule.ps1
+  #   https://github.com/alainQtec/PsCraft/blob/main/Public/ModuleManager/New-PSModule.ps1
   # .EXAMPLE
-  #   New-PsModule -Verbose
-  #   Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+  #   $m = New-PsModule
+  #   This example shows how to create a random module.
   # .Example
   #   Get-ChildItem *.ps1, *.psd1 -Recurse | New-PsModule MyUtility
   #   This example shows how to pipe the files into the New-PsModule, and yet another approach to collecting the files needed. [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "Medium", DefaultParameterSetName = "NewModuleManifest")]
@@ -19,61 +19,54 @@ function New-PsModule {
   param (
     # The Name Of your Module; note that it Should always match BaseName of its path.
     [Parameter(Position = 0, ParameterSetName = 'ByName')]
-    [ValidateScript({ if ($_ -match "[$([regex]::Escape(([io.path]::GetInvalidFileNameChars() -join '')))]") { throw "The ModuleName must be a valid folder name. The character '$($matches[0])' is not valid in a Module name." } else { $true } })]
-    [string]$Name,
-
-    [Parameter(Position = 0, ParameterSetName = 'ByConfig')]
-    [ValidateNotNullOrEmpty()]
-    [Array]$Configuration,
+    [ValidateScript({ if ($_ -match "[$([regex]::Escape(([IO.Path]::GetInvalidFileNameChars() -join '')))]") { throw "The ModuleName must be a valid folder name. The character '$($matches[0])' is not valid in a Module name." } else { $true } })]
+    [string]$Name = (cliHelper.core\New-RandomFileName).Replace('.', ''),
 
     # The FullPath Of your Module.
     [Parameter(Position = 1, Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = '__AllParameterSets')]
     [ValidateNotNullOrEmpty()]
     [string]$Path = '.',
 
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
     # The name of the author to use for the psd1 and copyright statement
-    [PSDefaultValue(Help = { "Env:UserName: (${Env:UserName})" })]
-    [String]$Author = $Env:UserName,
+    [PSDefaultValue(Help = { "ie: env:UserName" })]
+    [String]$Author = ([moduleManager]::GetAuthorName()),
 
-    # A short description of the contents of the module.
-    [Parameter(Position = 1)]
-    [PSDefaultValue(Help = { "'A collection of script files by ${Env:UserName}' (uses the value from the Author parmeter)" })]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
+    [PSDefaultValue(Help = { "'A collection of script files by UserName (uses the value from the Author parmeter)" })]
     [string]${Description} = "A collection of script files by $Author",
 
-    # The version of the module
-    # (This is a passthru for New-ModuleManifest)
-    [Parameter()]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
     [PSDefaultValue(Help = "1.0 (when -Upgrade is set, increments the existing value to the nearest major version number)")]
-    [Alias("Version", "MV")]
-    [Version]${ModuleVersion} = "1.0",
+    [Alias("Version", "MV")][Version]
+    ${ModuleVersion} = "1.0",
 
-    # (This is a passthru for New-ModuleManifest)
-    [AllowEmptyString()]
-    [String]$CompanyName = "None (Personal Module)",
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
+    [AllowEmptyString()][String]
+    $CompanyName = "None (Personal Module)",
 
-    # Specifies the minimum version of the Common Language Runtime (CLR) of the Microsoft .NET Framework that the module requires (Should be 2.0 or 4.0). Defaults to the (rounded) currently available ClrVersion.
-    # (This is a passthru for New-ModuleManifest)
-    [version]
-    [PSDefaultValue(help = { "Your current CLRVersion number (rounded): ($PSVersionTable.CLRVersion)" })]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
+    [PSDefaultValue(help = { "Your current CLRVersion number (rounded): ($PSVersionTable.CLRVersion)" })][version]
     ${ClrVersion} = $PSVersionTable.CLRVersion,
 
-    # Specifies the minimum version of Windows PowerShell that will work with this module. Defaults to 1 less than your current version.
-    # (This is a passthru for New-ModuleManifest)
-    [version]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
     [PSDefaultValue(Help = { "Your current PSVersion number (rounded): ($($PSVersionTable.PSVersion.ToString(2))" })]
-    [Alias("PSV")]
+    [version][Alias("PSV")]
     ${PowerShellVersion} = ("{0:F1}" -f [double]($PSVersionTable.PSVersion | Select-Object @{l = 'str'; e = { $_.Major.ToString() + '.' + $_.Minor.ToString() } }).str),
 
     # Specifies modules that this module requires. (This is a passthru for New-ModuleManifest)
-    [System.Object[]]
-    [Alias("Modules", "RM")]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
+    [System.Object[]][Alias("Modules", "RM")]
     ${RequiredModules} = $null,
 
     # Specifies the assembly (.dll) files that the module requires. (This is a passthru for New-ModuleManifest)
-    [AllowEmptyCollection()]
-    [string[]]
-    [Alias("Assemblies", "RA")]
-    ${RequiredAssemblies} = $null
+    [Parameter(Mandatory = $false, ParameterSetName = 'ByName')]
+    [string[]][Alias("Assemblies", "RA")][AllowEmptyCollection()]
+    ${RequiredAssemblies} = $null,
+
+    [Parameter(Position = 0, ParameterSetName = 'ByConfig')]
+    [ValidateNotNullOrEmpty()]
+    [Array]$Configuration
   )
 
   begin {
@@ -87,17 +80,35 @@ function New-PsModule {
     } else {
       $Module = [PsModule]::Create($Name)
     }
+    if ($PSCmdlet.ParameterSetName -eq 'ByName') {
+      @(
+        "Author",
+        "ClrVersion",
+        "Description",
+        "CompanyName",
+        "ModuleVersion",
+        "RequiredModules",
+        "PowerShellVersion",
+        "RequiredAssemblies"
+      ).ForEach({
+          $n = $_; $isBound = $PSCmdlet.MyInvocation.BoundParameters.ContainsKey($n)
+          $value = if ($isBound) { $PSCmdlet.MyInvocation.BoundParameters[$n] } else { Get-Variable -Name $n -ValueOnly }
+          Write-Verbose "$n = $value $(if(!$isBound){'(default)'})"
+          if ($null -ne $value) {
+            #ie: [void]$Module.Data.Where({ $_.Key -eq $n }).set_Value($value) # but faster.
+            [void]$Module.SetValue($_, $value)
+          }
+        }
+      )
+    } else {
+      # TODO: Add support for ByConfig
+      # $Module.Data = [PsModule]::CreateModuleData($configuration)
+      throw "Config params r not yet supported (WIP)"
+    }
     if ($PSCmdlet.ShouldProcess("", "", "Format and Write Module folder structure")) {
       [void]$Module.save()
       if ([IO.Directory]::Exists($Module.Path.FullName)) {
-        [string]$HostOs = [PsCraft]::GetHostOs()
-        if ($HostOs -in ("Linux", "MacOSX")) {
-          &tree -a $Module.Path.FullName | Out-Host
-        } elseif ($HostOs -eq "Windows") {
-          # TODO: Use the Show-Tree Cmdlet : WIP in github.com/alainQtec/cliHelper.core
-          # Here's a very half-ass version of it:
-          Get-ChildItem -Recurse $Path | ForEach-Object { $depth = ($_ | Split-Path -Parent | Split-Path -Leaf).Count; $(' ' * $depth * 2) + $_.Name | Write-Host -f Blue }
-        }
+        cliHelper.core\Show-Tree $Module.Path.FullName -Depth 5 | Out-Host
       }
     }
   }
